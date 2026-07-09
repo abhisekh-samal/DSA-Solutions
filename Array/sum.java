ade4c61f4915003ca3f6d3f5c3723135ca7fb793
Abhisekh samal <abhisheksamal8064@gmail.com>
2026-07-09 18:07:33 +0530

    Two sum

--- Array/sum.java
+++ Array/sum.java
@@ -1,4 +1,13 @@
-package Array;
 public class sum {
-    
+	public static int sum(int[] arr) {
+		if (arr == null) return 0;
+		int s = 0;
+		for (int v : arr) s += v;
+		return s;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4};
+		System.out.println(sum(a));
+	}
 }
